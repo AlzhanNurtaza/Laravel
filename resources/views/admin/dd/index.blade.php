ded2034f7154a6a14da69ea48f196ccf8b60594b
alzhannurtaza <alzhan.nurtaza@gmail.com>
2022-02-17 01:29:33 -0800

    ODDS charts added

--- resources/views/admin/dd/index.blade.php
+++ resources/views/admin/dd/index.blade.php
@@ -1,27 +1,205 @@
-@extends('layouts.admin')
+@extends('layouts.adminQlikCapability')
 @section('content')
-<div class="row">
-    <div class="card bg-white">
-        <div class="card-header border-b border-blueGray-200">
-            <div class="card-header-container">
-                <h6 class="card-title">
-                    {{ trans('cruds.dd.title') }}
-                </h6>
+    <div class="row m-0 ">
+        <div class="card bg-white mb-0 !rounded-none !rounded-t-lg">
+            <div class="card-header border-b border-blueGray-200">
+                <div class="card-header-container">
+                    <h6 class="card-title">
+                        {{ trans('cruds.dd.title') }}
+                    </h6>
+                </div>
             </div>
-        </div>
+            <div class="card-body !pb-0" style="min-height: 180px">
+                <div class="flex flex-wrap filterpane">
 
-        <div class="card-body">
-            <div class="pt-3">
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </p>
+                    <div class="qs-object w-6/12 lg:w-3/12" id="yCVxcU" style="height: 85px">
+                    </div>
+                    <div class="qs-object w-6/12 lg:w-3/12" id="42fae803-1154-4560-8db6-292eca9517a3" style="height: 85px">
+                    </div>
+                    <div class="qs-object w-full lg:w-3/12" id="3e6556a4-4f54-4ceb-b355-eee079c08c27" style="height: 85px">
+                    </div>
+                </div>
+                <div class="flex flex-wrap filterpane">
+                    <div class="qs-object w-6/12 lg:w-3/12" id="meycKf" style="height: 60px">
+                    </div>
+                    <div class="qs-object w-6/12 lg:w-3/12" id="vWZuPn" style="height: 60px">
+                    </div>
+                    <div class="qs-object w-6/12 lg:w-3/12" id="dVEwSsP" style="height: 60px">
+                    </div>
+                    <div class="qs-object w-6/12 lg:w-3/12" id="SaJjySN" style="height: 60px">
+                    </div>
+                </div>
+                <div class="flex flex-wrap filterpane">
+                    <div class="qs-object w-6/12 lg:w-1/12" id="37b977f0-ffae-4ab1-a419-383623fc37c5" style="height: 30px">
+                    </div>
+                    <div class="qs-object w-6/12 lg:w-1/12" id="e5afb703-aba3-4936-8efa-ca205cda2930" style="height: 30px">
+                    </div>
+                </div>
             </div>
         </div>
     </div>
-</div>
+    <div class="row m-0 sticky top-0  z-2" style="min-height: 40px">
+        <div class="flex flex-wrap filterpane">
+            <div class="w-full" id="CurrentSelections">
+            </div>
+        </div>
+    </div>
+
+
+    <div class="flex flex-wrap" id="wrapper-for-text-pink">
+        <div class="w-full">
+            <ul class="flex mb-0 list-none flex-wrap pt-3 p-4 flex-row">
+                <li class="-mb-px mr-2 last:mr-0 flex-auto text-center mt-4">
+                    <a class="text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal text-white bg-senseColor-600 cursor-pointer"
+                        data-tab-toggle="tab0" onclick="changeAtiveTab(event,'wrapper-for-text-pink','senseColor','tab0')">
+                        Сводная информация
+                    </a>
+                </li>
+                <li class="-mb-px mr-2 last:mr-0 flex-auto text-center mt-4">
+                    <a class="text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal text-senseColor-600 bg-white cursor-pointer"
+                        data-tab-toggle="tab1" onclick="changeAtiveTab(event,'wrapper-for-text-pink','senseColor','tab1')">
+                        Детализация
+                    </a>
+                </li>
+            </ul>
+            <div class="tab-content tab-space">
+                <div class="block" data-tab-content="true" id="tab0">
+                    <div class=" mx-auto w-full">
+                        <!-- Card stats -->
+                        <div class="flex flex-wrap ">
+                            <div class="w-full lg:w-4/12 xl:w-2/12 p-4 flex-auto">
+                                <div
+                                    class="relative flex flex-col min-w-0 break-words bg-white rounded mb-6 xl:mb-0 shadow-lg">
+                                    <div class="flex-auto p-4 qs-object-tab h-250-px" id="jbQCp">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="w-full lg:w-4/12 xl:w-2/12 p-4 flex-auto ">
+                                <div
+                                    class="relative flex flex-col min-w-0 break-words bg-white rounded mb-6 xl:mb-0 shadow-lg">
+                                    <div class="flex-auto p-4 qs-object-tab h-250-px" id="SAKvKKg">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="w-full lg:w-4/12 xl:w-2/12 p-4 flex-auto ">
+                                <div
+                                    class="relative flex flex-col min-w-0 break-words bg-white rounded mb-6 xl:mb-0 shadow-lg">
+                                    <div class="flex-auto p-4 qs-object-tab h-250-px" id="tyLyKTR">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="w-full lg:w-4/12 xl:w-2/12 p-4 flex-auto">
+                                <div
+                                    class="relative flex flex-col min-w-0 break-words bg-white rounded mb-6 xl:mb-0 shadow-lg">
+                                    <div class="flex-auto p-4 qs-object-tab h-250-px" id="htbFTu">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Blocks 1 -->
+                    <div class=" mx-auto w-full">
+                        <div class="flex flex-wrap">
+                            <div class="w-full lg:w-6/12  p-4">
+                                <div class="relative  break-words bg-white rounded mb-6 xl:mb-0 shadow-lg">
+                                    <div class="w-full p-4 qs-object-tab h-350-px lg:h-600-px" id="GsJuj">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="w-full lg:w-6/12  p-4">
+                                <div class="relative  break-words bg-white rounded mb-6 xl:mb-0 shadow-lg">
+                                    <div class="w-full p-4 qs-object-tab h-400-px lg:h-600-px" id="RxtmfH">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Blocks 2 -->
+                    <div class=" mx-auto w-full">
+                        <div class="flex flex-wrap">
+                            <div class="w-full lg:w-6/12  p-4">
+                                <div class="relative  break-words bg-white rounded mb-6 xl:mb-0 shadow-lg">
+                                    <div class="w-full p-4 qs-object-tab h-100-px" id="hjanH">
+                                    </div>
+                                </div>
+                                <div class="relative  break-words bg-white rounded mb-6 xl:mb-0 shadow-lg">
+                                    <div class="w-full p-4 qs-object-tab  h-350-px  lg:h-500-px" id="BpVr">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="w-full lg:w-6/12  p-4 hidden lg:block">
+                                <div class="relative  break-words bg-white rounded mb-6 xl:mb-0 shadow-lg">
+                                    <div class="w-full p-4 qs-object-tab h-350-px lg:h-600-px" id="cjfWj">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="hidden" data-tab-content="true" id="tab1">
+                    <div class=" mx-auto w-full">
+                        <!-- Card stats -->
+                        <div class="flex flex-wrap ">
+                            <div class="w-full lg:w-4/12 xl:w-2/12 p-4 flex-auto">
+                                <div
+                                    class="relative flex flex-col min-w-0 break-words bg-white rounded mb-6 xl:mb-0 shadow-lg">
+                                    <div class="flex-auto p-4 qs-object-tab h-250-px" id="NFqpAvq">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="w-full lg:w-4/12 xl:w-2/12 p-4 flex-auto ">
+                                <div
+                                    class="relative flex flex-col min-w-0 break-words bg-white rounded mb-6 xl:mb-0 shadow-lg">
+                                    <div class="flex-auto p-4 qs-object-tab h-250-px" id="RfPzP">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="w-full lg:w-4/12 xl:w-2/12 p-4 flex-auto ">
+                                <div
+                                    class="relative flex flex-col min-w-0 break-words bg-white rounded mb-6 xl:mb-0 shadow-lg">
+                                    <div class="flex-auto p-4 qs-object-tab h-250-px" id="QDggp">
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                    <!-- Blocks 1 -->
+                    <div class=" mx-auto w-full">
+                        <div class="flex flex-wrap">
+                            <div class="w-full lg:w-6/12  p-4">
+                                <div class="relative  break-words bg-white rounded mb-6 xl:mb-0 shadow-lg">
+                                    <div class="w-full p-4 qs-object-tab h-350-px lg:h-600-px" id="kDJAaup">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="w-full lg:w-6/12  p-4">
+                                <div class="relative  break-words bg-white rounded mb-6 xl:mb-0 shadow-lg">
+                                    <div class="w-full p-4 qs-object-tab h-400-px lg:h-600-px" id="BuZM">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Blocks 2 -->
+                    <div class=" mx-auto w-full">
+                        <div class="w-full   p-4 hidden lg:block">
+                            <div class="relative  break-words bg-white rounded mb-6 xl:mb-0 shadow-lg">
+                                <div class="w-full p-4 qs-object-tab h-350-px lg:h-600-px" id="FEtX">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+@section('QlikScripts')
+    qlikConnection('c1960e4c-1e89-4dd8-b147-2e3fd0f3447f','breeze',null);
+@endsection
 @endsection
